495e10eed108f0772f6488d1d52aa0f70de26498
justinxiang <liao1120x@gmail.com>
2022-11-07 19:10:00 +0800

    修改了首页代码

--- src/login.tsx
+++ src/login.tsx
@@ -12,46 +12,30 @@ const Login = () => {
   //确认登陆
   //确认登陆const
   //确认登陆login?username=${username}&password=${password}
-  const [loginOn, set_loginOn] = useState(false);
-  const [suc, set_suc] = useState(false);
-  const [nav, set_nav] = useState("/");
-  const [username, set_username] = useState("");
-  const [password, set_password] = useState("");
+  const [loginOn, setLoginOn] = useState(false);
+  const [suc, setSuc] = useState(false);
+  const [nav, setNav] = useState("/");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   // const []
   const route = useRef("");
   useEffect(() => {
-    // let username='zhsy1234'
-    // let password='123456'
-    // console.log(loginOn);
     if (loginOn) {
-      //判断按钮是否按下
-      //         let res=fetch(`http://region-11.autodl.com:14399/login?name=${username}&password=${password}`,
-      // {method:'POST', headers:{accept:'application/json'}}).then((res)=>{res.json().then((data)=>{
-      //   if(typeof(data.success)=='undefined'){set_suc(false);
-      //     console.log(data)}else{set_suc(true);console.log(data.success)}
-      // })})
-      // .catch((e)=>{console.log(e)});
-      //   let res=fetch('http://region-11.autodl.com:14399/login',
-      // {method:'POST',body:JSON.stringify({'username':'zhsy','password':'123456'})}).then((res)=>{console.log(res.status);console.log('ggg')});
-      // route.current='/'
-      set_suc(true);
-      
-      // set_loginOn(false);
+      console.log(loginOn);
+      setSuc(true);
     }
-  }, [loginOn, set_loginOn]);
+  }, [loginOn, setLoginOn]);
+
   useEffect(() => {
     if (suc) {
       //成功验证，进行跳转
-      alert("成功验证");
       console.log("right");
-      set_nav("/Main");
-      set_suc(false);
+      setNav("/Main");
+      setSuc(false);
     }
-  }, [suc, set_suc]);
+  }, [suc, setSuc]);
 
-  // {* <button type="submit" id="btn" onClick={()=>{set_loginOn(true)}}><></></button> *}
-  // <Link to='/Main'>确认登陆</Link>
   return (
     <>
       <head>
@@ -68,14 +52,14 @@ const Login = () => {
             <img src={logo} />
           </div>
           <div className="login-form">
-            <form action="#">
+            <form action="">
               <h2>登录</h2>
               <input
                 type="text"
                 name="username"
                 placeholder="用户名"
                 onChange={(e) => {
-                  set_username(e.target.value);
+                  setUsername(e.target.value);
                 }}
               ></input>
               <input
@@ -83,20 +67,22 @@ const Login = () => {
                 name="password"
                 placeholder="密码"
                 onChange={(e) => {
-                  set_password(e.target.value);
+                  setPassword(e.target.value);
                 }}
               ></input>
 
-              <div className="signUp">
-                <a href="/register">注册账号</a>
-              </div>
-              {/*<button><Link to="/UserInfo">用户信息</Link></button>*/}
+              <button type="submit" id="btn">
+                <Link to={"/register"} state={{ usr: username }}>
+                  注册为新用户
+                </Link>
+              </button>
 
+              <br />
               <button
                 type="submit"
                 id="btn"
                 onClick={() => {
-                  set_loginOn(true);
+                  setLoginOn(true);
                 }}
               >
                 <Link to={nav} state={{ usr: username }}>
